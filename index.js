c9c344845b94166a5162fe5854e9c1662b01d186
Jean Claude <demo.claude15@gmail.com>
2022-09-21 00:39:57 +0200

    mongoose connection

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ app.use("/comment", require("./routes/comment"));
 app.use("/announcement", require("./routes/announcement"));
 app.use(errorHandler);
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+});
 console.log("DB has been connected");
 
 app.listen(process.env.PORT || 3500, () => {
